954becfe325f7264d3f9b77923cea7e166b2f7a6
Sarah Sabongi <guimaraess737@gmail.com>
2024-06-13 16:56:00 -0300

    exercicios

--- projeto_lp3/projeto_lp3/app.py
+++ projeto_lp3/projeto_lp3/app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
+# Jeito errado de fazer:
+# from validate_docbr import CPF # importa a classe CPF
+# from validate_docbr import CNPJ # importa a classe CNPJ
+
+# Jeito correto de fazer:
+from validate_docbr import CNPJ, CPF 
+
 app = Flask("Minha App")
 
 # rota + função
@@ -17,6 +24,28 @@ def Contato():
 
 # /produtos - pagina produtos 
 
-@app.route("/Produtos")
+@app.route("/produtos")
 def Produtos():
     return "<h1>Produtos<h1>"
+
+# criar uma página /servicos retornar "nossos serviços" (colar oq ja tem)
+# página /gerar-cpf retornar cpf aleatório (usar a biblioteca do cpf que instalamos)
+# página /gerar cnpj e retornar Cnpj aleatório (usar a mesma biblioteca de cima) 
+
+@app.route("/servicos")
+def servicos():
+    return "<h1> Nossos serviços <h1>" # devolve HTML como STring, péssima pratica
+
+
+@app.route("/cpf")
+def gerarCpf():
+    cpf = CPF() # pode colocar dentro ou fora da função
+    return cpf.generate(True)
+
+@app.route("/cnpj")
+def gerarCnpj():
+    cnpj = CNPJ() # objeto do tipo CNPJ
+    return cnpj.generate(True)
+
+app.run()
+
--- projeto_lp3/projeto_lp3/cpf.py
+++ projeto_lp3/projeto_lp3/cpf.py
@@ -1,11 +1,11 @@
 from validate_docbr import CPF
 
-cpf = CPF()
+cpf = CPF() #objeto do tipo
 
-print(cpf.generate(True))
+print(cpf.generate(True)) #chamada #devolve com mascara
 print(cpf.generate(False))
 
-print(cpf.validate("510.242.307-08"))
+print(cpf.validate("510.242.307-08")) 
 print(cpf.validate("51024230708"))
 
 cpfs = [
